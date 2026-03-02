f0154138d7942d892ee31a151e0275b99d2e4f21
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-03-02 12:49:04 +0100

    BuildingViewModel started

---
+++ Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/BuildingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Budweg_KommeGaaSystem.Models;
+
+namespace Budweg_KommeGaaSystem.ViewModels
+{
+    public class BuildingViewModel
+    {
+        private Building building;
+        public string BuildingName { get { return building.BuildingName; } }
+
+        public BuildingViewModel(Building building)
+        {
+            this.building = building;
+        }
+    }
+}
