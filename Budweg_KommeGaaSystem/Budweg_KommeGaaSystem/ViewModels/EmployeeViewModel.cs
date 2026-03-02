5f8250725c51604e81d049403e3409f099b5749e
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-03-02 13:01:51 +0100

    EmployeeViewModel implemented

---
+++ Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Budweg_KommeGaaSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budweg_KommeGaaSystem.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        private Employee _employee;
+
+        public string EmployeeName { get; set; }
+
+        public EmployeeViewModel(Employee employee)
+        {
+            _employee = employee;
+            EmployeeName = $"{_employee.EmployeeId}: {_employee.FrontName} {_employee.LastName}";
+        }
+    }
+}
