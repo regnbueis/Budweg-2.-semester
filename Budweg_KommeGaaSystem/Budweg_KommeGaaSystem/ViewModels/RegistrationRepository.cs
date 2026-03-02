f46d873b77158e13ff545376fbf71921ad374a9e
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-03-02 12:37:45 +0100

    RegistrationRepo is not needed now, so not done yet.

--- Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/RegistrationRepository.cs
+++ Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/RegistrationRepository.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Budweg_KommeGaaSystem.Models;
+using Microsoft.Data.SqlClient;
 
 namespace Budweg_KommeGaaSystem.ViewModels
 {
-    class RegistrationRepository
+    public class RegistrationRepository
     {
+        private readonly string connectionString;
+        private List<Registration> registrations;
+
+        public RegistrationRepository()
+        {
+            connectionString = Configuration.ConnectionString;
+        }
+        public void CreateEmployeeArrival(int employee)
+        {
+            //IKKE IMPLEMENTERET ENDNU
+        }
+        public void UpdateEmployeeDeparture(int employee)
+        {
+            //IKKE IMPLEMENTERET ENDNU
+        }
+
+
     }
 }
