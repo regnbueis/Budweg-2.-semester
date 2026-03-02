1f55db9dd22b72b6aa2790fd64c00791ad98bc12
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-03-02 08:50:46 +0100

    EmployeeRepo start

--- Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/EmployeeRepository.cs
+++ Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/ViewModels/EmployeeRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Budweg_KommeGaaSystem.Models;
 
 namespace Budweg_KommeGaaSystem.ViewModels
 {
-    class EmployeeRepository
+    public class EmployeeRepository
     {
+        private readonly string connecionString;
+        private List<Employee> employees;
+
+        public EmployeeRepository()
+        {
+
+        }
     }
 }
