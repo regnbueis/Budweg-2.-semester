229665bb7cf2c402ab8aff0cf503d5747d6cd705
Mathias Sørensen <mathiassrensen514@gmail.com>
2026-03-04 11:14:50 +0100

    Configuration

--- Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/Configuration.cs
+++ Budweg_KommeGaaSystem/Budweg_KommeGaaSystem/Configuration.cs
@@ -7,10 +7,10 @@ namespace Budweg_KommeGaaSystem
 {
     internal static class Configuration
     {
-        private static IConfiguration configuration = new ConfigurationBuilder()
+        private static readonly IConfiguration configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
 
-        public static string ConnectionString = configuration.GetConnectionString("MyDBConnection") ?? "";
+        public static string ConnectionString { get; } = configuration.GetConnectionString("MyDBConnection") ?? "";
     }
 }
